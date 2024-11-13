0f855a679dd62bc87b088c56b0d5402908c0b2ee
KOL-kwn <kawanoschiro@gmail.com>
2024-11-13 16:41:38 -0300

    primeiro commit

---
+++ Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,39 @@
+namespace MauiAppHotell.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+    App PropriedadesApp;
+    public ContratacaoHospedagem()
+    {
+        InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+        
+        dtpck_checkin.MinimumDate = DateTime.Now;
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new HospedagemContratada());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
+    }
+}
---
+++ Views/Sobre.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotell.Views;
+
+public partial class Sobre : ContentPage
+{
+	public Sobre()
+	{
+		InitializeComponent();
+	}
+}
